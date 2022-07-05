53ffd5dacc9e0c0b4e74f7a25e122d3e16e2286b
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-05 18:39:30 -0300

    asd

--- index.php
+++ index.php
@@ -9,13 +9,12 @@ if(!$conn){
   echo "Connection error: " . mysqli_connect_error();
 }
 
-$sql = 'SELECT * FROM `venta` WHERE 1';
+$sql = 'SELECT * FROM `venta` WHERE 2';
 
 $resultado = mysqli_query($conn, $sql);
 
 $propiedades = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
 
-print_r($propiedades);
 ?>
 <head>
   <meta charset="utf-8">
