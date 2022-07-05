ddbc2e8ea5a61bdfb66b11cb85e9f1c40bb00800
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-05 18:59:29 -0300

    asd

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if(!$conn){
   echo "Connection error: " . mysqli_connect_error();
 }
 
-$sql = SELECT `NOMBRE` FROM `venta`;
+$sql = "SELECT `NOMBRE` FROM `venta`;";
 
 $resultado = mysqli_query($conn, $sql);
 
@@ -65,7 +65,7 @@ $propiedades = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
       <form class="cards" method="post">
         <div class="card">
           <div class="card-text">
-            <h3 name="titulo"><?php echo ($NOMBRE[1]) ?></h3>
+            <h3 name="titulo"><?php echo ($NOMBRE) ?></h3>
             <p>Descripción</p>
           </div>
         </div>
