4d76d78a6f7558d9eaa5838f8b2228192c087b93
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-05 20:45:39 -0300

    asd

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ $resultado = mysqli_query($conn, $sql);
           <div class="card-title">
             <h3 style="color:red" name="NOMBRE">Titulo HTML
               <?php
-                echo "titulo PHP"
+                echo "titulo PHP";
               ?>
             </h3>
           </div>
