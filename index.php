17a18ce575fa03e51226ba262c2b7bd7bf4042af
Marcos <marcoszinga_1994@hotmail.com>
2022-07-06 01:54:13 -0300

    asd

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ $alquiler = "SELECT * FROM alquiler ORDER BY `alquiler`.`FOTO` DESC";
             <?php $resultado = mysqli_query($conn, $venta);
             while ($row=mysqli_fetch_assoc($resultado)) {?>
             <div class="card">
-                <div class="foto"><img src="<?php echo $row['FOTO'];?>" alt="imagen">"</div>
+              <div class="foto"><img src="<?php echo $row['FOTO'];?>" alt="imagen">"</div>
               <div class="nombre"><p><?php echo $row["TIPO"];?></p></div>
               <div class="descripcion"><p><?php echo $row["DESCRIPCION"];?></p></div>
               <div class="precio"><p>$ <?php echo $row["PRECIO"];?></p></div>
