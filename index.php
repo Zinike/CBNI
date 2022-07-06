cf4d5d9d8d2e28841b22dd26988b3f63f5ddd846
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-06 11:52:20 -0300

    asd

--- index.php
+++ index.php
@@ -66,6 +66,9 @@ $alquiler = "SELECT * FROM `alquiler` ORDER BY `alquiler`.`FOTO` DESC";
             <?php }?>
             <?php mysqli_free_result($resultado);?>
         </form>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
 
         <h3>Venta</h3>
         <form class="cards">
