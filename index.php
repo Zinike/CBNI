67e32e529b7c4cefe839539b80fb6cf147a72a67
Marcos <marcoszinga_1994@hotmail.com>
2022-07-05 22:04:08 -0300

    asd

--- index.php
+++ index.php
@@ -55,14 +55,16 @@ $alquiler = "SELECT * FROM alquiler";
       <div class="contenedor">
         <h2>Nuestras Propiedades</h2>
         <form class="cards" method="post">
-          <div class="card">
-            <?php
-              $resultado = mysqli_query($conn, $venta);
-              while ($row = myslqi_fetch_assoc($resultado)) {
-            ?>
-              <div style="color:red" class="titulo"><?php echo $row["NOMBRE"];?></div>
-              <div class="descripcion"><?php echo $row["DESCRIPCION"];?></div>
-            <?php };?>
+          <div class="card">Hola?
+
+              <?php
+                $resultado = mysqli_query($conn, $venta);
+                while ($row = myslqi_fetch_assoc($resultado)) {?>
+
+                <div style="color:red" class="titulo"><?php echo nombre;?></div>
+                <div class="descripcion"><?php echo descripcion;?></div>
+              <?php };?>
+
           </div>
         </form>
       </div>
