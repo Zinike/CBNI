cc6d6cc62c158b97b9eb34a59b7e1c981c769bb1
Marcos <marcoszinga_1994@hotmail.com>
2022-07-07 00:44:09 -0300

    asd

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     </header>
 
     <section id="hero">
-      <video src="images/realstate.mp4" poster="default.jpg" muted autoplay loop z-index="1"></video>
+      <video src="images/realstate.mp4" poster="default.jpg" muted autoplay loop width="100%" height="100%"></video>
       <div class="hero">
         <img class="logo2" src="images/logo2.png" alt="logo">
       </div>
