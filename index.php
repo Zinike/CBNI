8176621d904621c4005455a1bfd9699cf5b15476
Marcos <marcoszinga_1994@hotmail.com>
2022-07-07 01:17:32 -0300

    asd

--- index.php
+++ index.php
@@ -32,10 +32,7 @@
     </header>
 
     <section id="hero">
-      <video src="images/realstate.mp4" poster="default.jpg" muted autoplay loop ></video>
-      <div class="hero">
-        <img class="logo2" src="images/logo2.png" alt="logo">
-      </div>
+      <video src="images/realstate.webm" poster="default.jpg" muted autoplay loop ></video>
     </section>
 
     <section id="nosotros">
