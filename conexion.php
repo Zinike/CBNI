9d01ad2f11566176ce03f2dc33316f3adcf71a40
Marcos <marcoszinga_1994@hotmail.com>
2022-07-09 00:10:23 -0300

    ASD

--- conexion.php
+++ conexion.php
@@ -4,6 +4,6 @@ if(!$conn){
   echo "Connection error: " . mysqli_connect_error();
 }
 
-$venta = "SELECT * FROM `venta`";
-$alquiler = "SELECT * FROM `alquiler`";
+$venta = "SELECT * FROM `venta` ORDER BY `venta`.`LOCALIDAD` ASC";
+$alquiler = "SELECT * FROM `alquiler` ORDER BY `alquiler`.`LOCALIDAD` ASC";
 ?>
