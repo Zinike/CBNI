0116206ccad1952469c4a9a126aa299413e56c63
Marcos <marcoszinga_1994@hotmail.com>
2022-07-27 01:04:15 -0300

    asd

--- cotizacion.js
+++ cotizacion.js
@@ -1,4 +1,10 @@
-const request = fetch('https://api-dolar-argentina.herokuapp.com/api/dolarblue')
-request.then(function(response) {
-  return response();
-})
+const request = async (url='https://api-dolar-argentina.herokuapp.com/api/dolarblue') => {
+  const response = await fetch(url);
+  if (!response.ok)
+    throw new Error("WARN", response.status);
+  const data = await response.text();
+  return data;
+}
+
+const resultOk = await request("/robots.txt");
+const resultError = await request("/nonExistentFile.txt");
