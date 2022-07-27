b90a7785d4117273b54cc7c2811b18b248f78504
Marcos <marcoszinga_1994@hotmail.com>
2022-07-27 00:28:11 -0300

    asd

--- cotizacion.js
+++ cotizacion.js
@@ -1,5 +1,5 @@
-const datos=>{
+async function datos() {
   await fetch("https://www.dolarsi.com/api/api.php?type=dolar");
 }
-
+console.log(datos)
 document.getElementById("respuesta").innerHTML = datos;
