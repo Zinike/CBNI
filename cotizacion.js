1316a0be00e44da2c12ea20d4e01bc4b0697dcae
Marcos <marcoszinga_1994@hotmail.com>
2022-07-26 23:54:04 -0300

    asd

--- cotizacion.js
+++ cotizacion.js
@@ -1,53 +1,17 @@
 async function postData(url = 'https://www.dolarsi.com/api/api.php?type=dolar', data = {}) {
 
 console.log("hola");
-  const response = await fetch(url, {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-postData('https://example.com/answer', { answer: 42 })
-  .then(data => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 var cotizaciones = {};
-var llamada = function(datos){
+var llamada = datos =>{
   console.log(datos);
   cotizaciones.dolar=datos;
   var html = "";
   html+="<h3>Dolar</h3>";
-  cotizaciones.dolar
+  cotizaciones.dolar.map(dolar=>{
+    hmtl+="Hola Dolar";
+  })
 
 
   document.getElementById("respuesta").innerHTML = hmtl;
