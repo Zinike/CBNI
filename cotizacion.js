cd89ad3127faeb085606ae5d9436a3da57c8be27
Marcos <marcoszinga_1994@hotmail.com>
2022-07-26 23:43:32 -0300

    asd

--- cotizacion.js
+++ cotizacion.js
@@ -1,9 +1,8 @@
 var cotizaciones = {};
 var casa = function(datos){
   console.log(datos);
+  cotizaciones.dolar=datos;
+  var html = "";
+  html += "<h3>Dolar</h3>";
+
 }
-
-
-
-
-document.getElementById("respuesta").innerHTML = "Dolar Blue";
