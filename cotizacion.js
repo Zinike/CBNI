030ae53ef12d2aa106bc6359e1551a36033a1f99
Marcos <marcoszinga_1994@hotmail.com>
2022-07-27 00:53:46 -0300

    asd

--- cotizacion.js
+++ cotizacion.js
@@ -1,5 +1,9 @@
 async function obtencionDatos() {
   const datos = await fetch("https://api-dolar-argentina.herokuapp.com/api/dolarblue");
+  .then(respuesta=> {
+    console.log(respuesta);
+  })
 }
 
-document.getElementById("respuesta").innerHTML = obtencionDatos.datos;
+
+document.getElementById("respuesta").innerHTML = obtencionDatos;
