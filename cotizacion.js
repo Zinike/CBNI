58dc880123ad8881d1dd29a9c9dadb9a4b004221
Marcos <marcoszinga_1994@hotmail.com>
2022-07-27 00:29:41 -0300

    asd

--- cotizacion.js
+++ cotizacion.js
@@ -1,5 +1,5 @@
 async function datos() {
   await fetch("https://www.dolarsi.com/api/api.php?type=dolar");
 }
-console.log(datos.casa);
+console.log(datos.dolar);
 document.getElementById("respuesta").innerHTML = datos;
